4d50558b0b61348d5d7eb6ab108b328525c90c59
luobin <1114513874@qq.com>
2017-08-16 18:30:40 +0800

    statrt

--- src/main/java/com/bin/MaitqzApplication.java
+++ src/main/java/com/bin/MaitqzApplication.java
@@ -2,9 +2,16 @@ package com.bin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MaitqzApplication {
+public class MaitqzApplication extends SpringBootServletInitializer{
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(MaitqzApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MaitqzApplication.class, args);
